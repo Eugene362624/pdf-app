f250a084e1f778ad56b21e6fe1d26b5f7052d712
Eugene <eugenemedvedevbelarus@gmail.com>
2020-10-13 19:50:14 +0300

    add build folder

---
+++ frontend/build/precache-manifest.1759184d61954bcce0644630f14095e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5601fd7a845814074f3a3829033d7e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "971adbd48dc69cca3f97",
+    "url": "/static/css/main.374b2eb6.chunk.css"
+  },
+  {
+    "revision": "224c68a636943390612a",
+    "url": "/static/js/2.7f2fb2a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7f2fb2a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "971adbd48dc69cca3f97",
+    "url": "/static/js/main.93764376.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
