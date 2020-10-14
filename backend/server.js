3b7628c6500a3b42c1575751319c623713a9dc75
Eugene <eugenemedvedevbelarus@gmail.com>
2020-10-14 23:39:16 +0300

    heroku3

--- backend/server.js
+++ backend/server.js
@@ -109,6 +109,8 @@ app.post('/create-pdf', (req, res) => {
 
 app.get('/fetch-pdf', (req, res) => {
   res.set('Access-Control-Allow-Origin', '*')
+  res.set('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.set('Access-Control-Allow-Headers', 'Content-Type')
   res.sendFile(`${__dirname}/${fileName}.pdf`)
 })
 
