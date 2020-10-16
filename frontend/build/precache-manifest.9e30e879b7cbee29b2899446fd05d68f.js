0debdd2a37c9c4c865ef7d796c65f68245236cce
Eugene <eugenemedvedevbelarus@gmail.com>
2020-10-16 14:01:11 +0300

    trying to be the best for the 4th day :( 1.2

--- frontend/build/precache-manifest.9e30e879b7cbee29b2899446fd05d68f.js
+++ frontend/build/precache-manifest.9e30e879b7cbee29b2899446fd05d68f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d0de46ea63e9cb1810440fab69be4065",
+    "revision": "bd4573ad814e2edf2e1450338ce2cbb7",
     "url": "/index.html"
   },
   {
-    "revision": "a180e73c48f24317ec9d",
+    "revision": "7902a94843ffe328a079",
     "url": "/static/css/main.374b2eb6.chunk.css"
   },
   {
-    "revision": "9b04a046f392d17ef31d",
-    "url": "/static/js/2.3f2cc560.chunk.js"
+    "revision": "71c43eea9d6bc6cfd142",
+    "url": "/static/js/2.bae08dbc.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.3f2cc560.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.bae08dbc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a180e73c48f24317ec9d",
-    "url": "/static/js/main.4259d776.chunk.js"
+    "revision": "7902a94843ffe328a079",
+    "url": "/static/js/main.fa5c04e1.chunk.js"
   },
   {
     "revision": "2747b914e9a60db2276f",
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c7318a78c8a9457228f667f4040818db.js"
+  "/precache-manifest.9e30e879b7cbee29b2899446fd05d68f.js"
 );
 
 self.addEventListener('message', (event) => {
