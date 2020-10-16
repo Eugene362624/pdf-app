260ac366cf476597b40b81b29f52e884948e7ea0
Eugene <eugenemedvedevbelarus@gmail.com>
2020-10-16 13:52:59 +0300

    trying to be the best for the 4th day :( 1.1

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json())
 // app.use(cors())
 app.use(parser.urlencoded({extended: true}))
 app.use(parser.json())
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', '*')
