bcac95845b5ce42df778787988c514448dc07aa8
Eugene <eugenemedvedevbelarus@gmail.com>
2020-10-16 17:40:27 +0300

    trying to be the best for the 4th day :( 1.5.5.7

--- backend/server.js
+++ backend/server.js
@@ -124,11 +124,6 @@ app.post('/create-pdf', async (req, res) => {
 })
 
 app.get('/fetch-pdf', (req, res) => {
-  
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', '*')
-  res.setHeader('Access-Control-Allow-Credentials', true)
   res.sendFile(`${__dirname}/${fileName}.pdf`)
   .catch((error) => {
     console.log(error);
